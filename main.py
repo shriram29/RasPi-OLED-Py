75dcac5664cc2ab8cc1adb844e62734950c67488
Shriram <shriram29199@gmail.com>
2020-01-05 18:30:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ from PIL import Image
 import qrcode
 
 
-
+def displayIMG(logo):
+    logo.thumbnail(device.size,Image.ANTIALIAS)
+    fff = Image.new(logo.mode, logo.size, (255,) * 4)
+    background = Image.new("RGBA", device.size, "black")
+    posn = ((device.width - logo.width) // 2, 0)
+    rot = logo.rotate(0, resample=Image.BILINEAR)
+    img = Image.composite(rot, fff, rot)
+    background.paste(img, posn)
+    device.display(background.convert(device.mode))
 
 def main():
     while True:
@@ -20,16 +28,7 @@ def main():
         qr.make(fit=True)
         img = qr.make_image(fill_color="white", back_color="black")
         logo=img.convert("RGBA")
-        logo.thumbnail(device.size,Image.ANTIALIAS)
-        fff = Image.new(logo.mode, logo.size, (255,) * 4)
-        background = Image.new("RGBA", device.size, "black")
-        posn = ((device.width - logo.width) // 2, 0)
-        rot = logo.rotate(0, resample=Image.BILINEAR)
-        img = Image.composite(rot, fff, rot)
-        background.paste(img, posn)
-        device.display(background.convert(device.mode))
-
-
+        displayIMG(logo)
 
 if __name__ == "__main__":
     try:
