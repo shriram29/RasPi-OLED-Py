965d533c61caebbca22dfc0f98b52bbec54ddc2f
Shriram <shriram29199@gmail.com>
2020-01-05 17:30:45 +0530

    Update main.py

---
+++ main.py
@@ -0,0 +1,23 @@
+import os.path
+from demo_opts import get_device
+from PIL import Image
+import qrcode
+
+
+
+def main():
+    while True:
+        qrstr=input("Input String : ")
+        logo=qrcode.make(qrstr)
+        fff = Image.new(logo.mode, logo.size, (255,) * 4)
+        background = Image.new("RGBA", device.size, "white")
+        posn = ((device.width - logo.width) // 2, 0)
+
+
+
+if __name__ == "__main__":
+    try:
+        device = get_device()
+        main()
+    except KeyboardInterrupt:
+        pass
